aa5c4775631f43fbf297760422c178e21bc302ea
Luis Gallardo <lgallard@gmail.com>
2014-04-29 18:32:36 -0430

    The service class was simplified.

--- qBittorrentClient/src/com/lgallardo/qbittorrentclient/qBittorrentService.java
+++ qBittorrentClient/src/com/lgallardo/qbittorrentclient/qBittorrentService.java
@@ -2,26 +2,24 @@ package com.lgallardo.qbittorrentclient;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
 
 public class qBittorrentService extends Service {
+
+	private qBittorrentBinder binder = null;
 
 	public void OnCreate() {
 		super.onCreate();
 
-		// private qBittorrentBinder binder=null;
-
 		// Binder
-		
-		//binder=new qBittorrentBinder();
-
-
+		binder = new qBittorrentBinder();
 	}
 
 	@Override
 	public IBinder onBind(Intent i) {
-		// TODO Auto-generated method stub
-		return null;
+		return (binder);
 	}
-
+	
 }
