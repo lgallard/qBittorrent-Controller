d9d037d1df67f74f6262ff45bc1b4142e3f8c69c
Luis M. Gallardo D. <lgallard@gmail.com>
2014-05-28 23:40:08 -0430

    Aproach discarded

--- qBittorrentClient/src/com/lgallardo/qbittorrentclient/qBittorrentListener.java
+++
@@ -1,8 +0,0 @@
-package com.lgallardo.qbittorrentclient;
-
-import com.lgallardo.qbittorrentclient.torrent;
-
-public interface qBittorrentListener {
-	void updateUI(torrent[] result);
-	void sendCommandResult(String result);
-}
