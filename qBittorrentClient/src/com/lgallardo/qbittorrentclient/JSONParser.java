759bee2b90b2adffeef4467f2cb5025c61b879ef
Luis Gallardo <lgallard@gmail.com>
2014-09-28 01:27:17 -0430

    * Increased timeouts values to qBittorrent servers

--- qBittorrentClient/src/com/lgallardo/qbittorrentclient/JSONParser.java
+++ qBittorrentClient/src/com/lgallardo/qbittorrentclient/JSONParser.java
@@ -95,11 +95,11 @@ public class JSONParser {
 
 		// Set the timeout in milliseconds until a connection is established.
 		// The default value is zero, that means the timeout is not used.
-		int timeoutConnection = 3000;
+		int timeoutConnection = 5000;
 
 		// Set the default socket timeout (SO_TIMEOUT)
 		// in milliseconds which is the timeout for waiting for data.
-		int timeoutSocket = 5000;
+		int timeoutSocket = 8000;
 
 		// Set http parameters
 		HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
@@ -178,11 +178,11 @@ public class JSONParser {
 
 		// Set the timeout in milliseconds until a connection is established.
 		// The default value is zero, that means the timeout is not used.
-		int timeoutConnection = 3000;
+		int timeoutConnection = 5000;
 
 		// Set the default socket timeout (SO_TIMEOUT)
 		// in milliseconds which is the timeout for waiting for data.
-		int timeoutSocket = 5000;
+		int timeoutSocket = 8000;
 
 		// Set http parameters
 		HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
