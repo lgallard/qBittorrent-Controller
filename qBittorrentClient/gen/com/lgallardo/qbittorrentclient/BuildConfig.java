519e5f5b1d41972d5ab1f624326ffa3a1fb77235
Luis Gallardo <lgallard@gmail.com>
2014-11-26 16:02:27 -0430

    * Changed layouts for better distirbution in phone and tablets * Updates strings

--- qBittorrentClient/gen/com/lgallardo/qbittorrentclient/BuildConfig.java
+++ qBittorrentClient/gen/com/lgallardo/qbittorrentclient/BuildConfig.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ *  Copyright (c) 2014 Luis M. Gallardo D..
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ *  
+ * Contributors:
+ *      Luis M. Gallardo D.
+ *******************************************************************************/
 /** Automatically generated file. DO NOT MODIFY */
 package com.lgallardo.qbittorrentclient;
 
