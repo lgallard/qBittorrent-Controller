e25bf34663d59ad0a1e272233765eb973fbadd97
Luis Gallardo <luis.gallardo@smartmatic.com>
2014-12-23 12:50:46 -0430

    *  Fixed "Share ratio" (and others) translated text not showing

--- qBittorrentClient/src/com/lgallardo/qbittorrentclient/AboutFragment.java
+++ qBittorrentClient/src/com/lgallardo/qbittorrentclient/AboutFragment.java
@@ -4,9 +4,9 @@
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- * 
+ *
  * Contributors:
- *     Luis M. Gallardo D. - initial implementation
+ *     Luis M. Gallardo D.
  ******************************************************************************/
 package com.lgallardo.qbittorrentclient;
 
