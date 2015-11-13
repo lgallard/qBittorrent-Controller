9de0131f2a5a1e0fc5076c67bfecf975a7383f82
Luis Gallardo <luis.gallardo@smartmatic.com>
2015-11-13 19:19:57 -0430

    * Changed license to GNU Lesser General Public License v3.0, in order to be compatible with Google libraries.

--- app/src/main/java/com/lgallardo/qbittorrentclient/MainActivity.java
+++ app/src/main/java/com/lgallardo/qbittorrentclient/MainActivity.java
@@ -1,14 +1,10 @@
-/**
- * ****************************************************************************
- * Copyright (c) 2014 Luis M. Gallardo D..
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v3.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/gpl.html
- * <p>
- * Contributors:
- * Luis M. Gallardo D.
- * ****************************************************************************
+/*
+ *   Copyright (c) 2014-2015 Luis M. Gallardo D.
+ *   All rights reserved. This program and the accompanying materials
+ *   are made available under the terms of the GNU Lesser General Public License v3.0
+ *   which accompanies this distribution, and is available at
+ *   http://www.gnu.org/licenses/lgpl.html
+ *
  */
 package com.lgallardo.qbittorrentclient;
 
