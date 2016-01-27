a349fab2fd0e5d42ad3d9370b12a1b5e2a7477ea
Luis Gallardo <luis.gallardo@smartmatic.com>
2016-01-27 18:40:27 -0430

    * Removed delay for download first and last parts, and sequential downloads

--- app/src/main/java/com/lgallardo/qbittorrentclient/MainActivity.java
+++ app/src/main/java/com/lgallardo/qbittorrentclient/MainActivity.java
@@ -2940,12 +2940,10 @@ public class MainActivity extends AppCompatActivity implements RefreshListener {
 
             if ("toggleFirstLastPiecePrio".equals(command)) {
                 messageId = R.string.torrentstogglefisrtLastPiecePrio;
-                delay = 3;
             }
 
             if ("toggleSequentialDownload".equals(command)) {
                 messageId = R.string.torrentstoggleSequentialDownload;
-                delay = 3;
             }
 
             if ("toggleAlternativeSpeedLimits".equals(command)) {
