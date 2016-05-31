4f7aee92d041ca63480e450df1c7d10e64146989
Luis Gallardo <luis.gallardo@smartmatic.com>
2016-05-31 13:13:53 -0400

    *  Redefined constructor to adapt to file a tracker items * Added Trackers type in getItemViewType method

--- app/src/main/java/com/lgallardo/qbittorrentclient/TorrentDetailsRecyclerViewAdapter.java
+++ app/src/main/java/com/lgallardo/qbittorrentclient/TorrentDetailsRecyclerViewAdapter.java
@@ -103,7 +103,7 @@ public class TorrentDetailsRecyclerViewAdapter extends RecyclerView.Adapter<Torr
     }
 
 
-    TorrentDetailsRecyclerViewAdapter(Context context, MainActivity mainActivity, ArrayList<TorrentDetailsItem> serverItems, ArrayList<TorrentDetailsItem> actionItems, ArrayList<TorrentDetailsItem> settingsItems, ArrayList<TorrentDetailsItem> labelItems) {
+    TorrentDetailsRecyclerViewAdapter(Context context, MainActivity mainActivity, ArrayList<TorrentDetailsItem> fileItems, ArrayList<TorrentDetailsItem> trackerItems) {
 
         this.mainActivity = mainActivity;
         this.context = context;
@@ -111,65 +111,14 @@ public class TorrentDetailsRecyclerViewAdapter extends RecyclerView.Adapter<Torr
 
         // All items
 
-        TorrentDetailsRecyclerViewAdapter.serverItems = serverItems;
-        TorrentDetailsRecyclerViewAdapter.actionItems = actionItems;
-        TorrentDetailsRecyclerViewAdapter.settingsItems = settingsItems;
-        TorrentDetailsRecyclerViewAdapter.labelItems = labelItems;
+        TorrentDetailsRecyclerViewAdapter.fileItems = fileItems;
+        TorrentDetailsRecyclerViewAdapter.trackerItems = trackerItems;
 
         TorrentDetailsRecyclerViewAdapter.items = new ArrayList<TorrentDetailsItem>();
 
         // Add items
-        TorrentDetailsRecyclerViewAdapter.items.addAll(serverItems);
-        TorrentDetailsRecyclerViewAdapter.items.addAll(actionItems);
-        TorrentDetailsRecyclerViewAdapter.items.addAll(settingsItems);
-
-        if (labelItems != null) {
-            TorrentDetailsRecyclerViewAdapter.items.addAll(labelItems);
-        } else {
-            TorrentDetailsRecyclerViewAdapter.labelItems = new ArrayList<TorrentDetailsItem>();
-        }
-
-//        Log.d("Debug", "DrawerItemRecyclerViewAdapter - Constructor - serverItems size: " + TorrentDetailsItemRecyclerViewAdapter.serverItems.size());
-//        Log.d("Debug", "DrawerItemRecyclerViewAdapter - Constructor - actionItems size: " + TorrentDetailsItemRecyclerViewAdapter.actionItems.size());
-//        Log.d("Debug", "DrawerItemRecyclerViewAdapter - Constructor - settingsItems size: " + TorrentDetailsItemRecyclerViewAdapter.settingsItems.size());
-//        Log.d("Debug", "DrawerItemRecyclerViewAdapter - Constructor - labelItems size: " + TorrentDetailsItemRecyclerViewAdapter.labelItems.size());
-//        Log.d("Debug", "DrawerItemRecyclerViewAdapter - Constructor - items size: " + TorrentDetailsItemRecyclerViewAdapter.items.size());
-
-//        drawerOffset = 1;
-
-        TorrentDetailsItem recyclerItem;
-
-//        // Add server items to array
-//        for (int i = 0; i < items.size(); i++) {
-//            ObjectDrawerItem item = items.get(i);
-//
-//            Log.d("Debug", "DrawerItemRecyclerViewAdapter - OnClick() - Adding to items: " + items.get(i).name);
-//
-//            if (item.getType() == TYPE_SERVER || item.getType() == TYPE_SERVER_ACTIVE) {
-//                serverItems.add(item);
-//            }
-//        }
-
-
-        // Remove all server items
-        Iterator iterator = items.iterator();
-
-        while (iterator.hasNext()) {
-
-            TorrentDetailsItem item = (TorrentDetailsItem) iterator.next();
-
-//            Log.d("Debug", "DrawerItemRecyclerViewAdapter - OnClick() - Analysing: " + item.name);
-//            Log.d("Debug", "DrawerItemRecyclerViewAdapter - OnClick() - Action is: " + item.getAction());
-
-//
-//            if (item.getType() == TYPE_SERVER || item.getType() == TYPE_SERVER_ACTIVE) {
-//
-////                Log.d("Debug", "DrawerItemRecyclerViewAdapter - OnClick() - Removing: " + item.name);
-//                iterator.remove();
-//            }
-
-
-        }
+        TorrentDetailsRecyclerViewAdapter.items.addAll(fileItems);
+        TorrentDetailsRecyclerViewAdapter.items.addAll(trackerItems);
 
 
     }
@@ -182,11 +131,6 @@ public class TorrentDetailsRecyclerViewAdapter extends RecyclerView.Adapter<Torr
 
     @Override
     public TorrentDetailsRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-//        Log.d("Debug", "DrawerItemRecyclerViewAdapter - OnClick() - ViewType: " + viewType);
-//        if (viewType == TYPE_SERVER || viewType == TYPE_SERVER_ACTIVE) {
-//            drawerOffset = drawerOffset + 1;
-//        }
 
         //inflate your layout and pass it to view holder
         if (viewType == TYPE_FILE_ITEM) {
@@ -213,25 +157,7 @@ public class TorrentDetailsRecyclerViewAdapter extends RecyclerView.Adapter<Torr
     // which view type is being created 1 for item row
     @Override
     public void onBindViewHolder(TorrentDetailsRecyclerViewAdapter.ViewHolder holder, int position) {
-//        if (holder.Holderid == 1) {                              // as the list view is going to be called after the header view so we decrement the
-//            // position by 1 and pass it to the holder while setting the text and image
-//
-//
-//            TorrentDetailsItem item = items.get(position - 1);
-//
-//            holder.imageViewIcon.setImageResource(item.icon);
-//            holder.textViewName.setText(item.name);
-//
-//
-//            holder.positionInItems = (position - 1);
-//
-//
-//        } else {
-//
-//            // header
-//
-//            return;
-//        }
+
     }
 
 
@@ -243,62 +169,20 @@ public class TorrentDetailsRecyclerViewAdapter extends RecyclerView.Adapter<Torr
     }
 
 
-    // Witht the following method we check what type of view is being passed
+    // With the following method we check what type of view is being passed
     @Override
     public int getItemViewType(int position) {
 
 //        Log.d("Debug", "DrawerItemRecyclerViewAdapter - items.size(): " + items.size());
 //        Log.d("Debug", "DrawerItemRecyclerViewAdapter - position: " + position);
 
-//        if (isPositionHeader(position)) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_HEADER");
-//            return TYPE_HEADER;
-//        }
-
-//        if (items.get(position - 1).getType() == TYPE_CATEGORY && !(items.get(position - 1).isActive())) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_CATEGORY");
-//            return TYPE_CATEGORY;
-//        }
-
-//        if (items.get(position - 1).getType() == TYPE_CATEGORY && items.get(position - 1).isActive()) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_CATEGORY");
-//            return TYPE_CATEGORY;
-//        }
-//
-//        if (items.get(position - 1).getType() == TYPE_LABEL_CATEGORY && !(items.get(position - 1).isActive())) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_LABEL_CATEGORY");
-//            return TYPE_LABEL_CATEGORY;
-//        }
-//
-//        if (items.get(position - 1).getType() == TYPE_LABEL_CATEGORY && items.get(position - 1).isActive()) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_LABEL_CATEGORY");
-//            return TYPE_LABEL_CATEGORY;
-//        }
-//
-//
-//        if (items.get(position - 1).getType() == TYPE_SERVER && !(items.get(position - 1).isActive())) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_SERVER");
-//            return TYPE_SERVER;
-//        }
-//
-//        if (items.get(position - 1).getType() == TYPE_SERVER && items.get(position - 1).isActive()) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_SERVER_ACTIVE");
-//            return TYPE_SERVER_ACTIVE;
-//        }
-//
-//
-//        if (items.get(position - 1).getType() == TYPE_LABEL && !(items.get(position - 1).isActive())) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_LABEL");
-//            return TYPE_LABEL;
-//        }
-//
-//        if (items.get(position - 1).getType() == TYPE_LABEL && items.get(position - 1).isActive()) {
-////            Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_LABEL_ACTIVE");
-//            return TYPE_LABEL_ACTIVE;
-//        }
+        if (items.get(position).getType() == TYPE_TRACKER_ITEM) {
+            Log.d("Debug", "TorrentDetailsRecyclerViewAdapter - TYPE_TRACKER_ITEM");
+            return TYPE_TRACKER_ITEM;
+        }
 
         // Default
-//        Log.d("Debug", "DrawerItemRecyclerViewAdapter - TYPE_FILE_ITEM");
+        Log.d("Debug", "TorrentDetailsRecyclerViewAdapter - TYPE_FILE_ITEM");
         return TYPE_FILE_ITEM;
 
     }
