07f7f30fb84e256a388fc1ac0bfe2723574913e7
Luis Gallardo <lgallard@gmail.com>
2018-02-28 17:17:43 -0300

    * Added toggleSequentialDownload using Volley

--- app/src/main/java/com/lgallardo/qbittorrentclient/MainActivity.java
+++ app/src/main/java/com/lgallardo/qbittorrentclient/MainActivity.java
@@ -2107,7 +2107,7 @@ public class MainActivity extends AppCompatActivity implements RefreshListener {
 
     }
 
-    private void toggleFirstLastPiecePriority(final String hashes, final VolleyCallback callback) {
+    private void toggleFirstLastPiecePrio(final String hashes, final VolleyCallback callback) {
 
         String url = "";
 
@@ -2174,6 +2174,76 @@ public class MainActivity extends AppCompatActivity implements RefreshListener {
         addVolleyRequest(jsArrayRequest);
 
     }
+
+    private void toggleSequentialDownload(final String hashes, final VolleyCallback callback) {
+
+        String url = "";
+
+        // if server is publish in a subfolder, fix url
+        if (subfolder != null && !subfolder.equals("")) {
+            url = subfolder + "/" + url;
+        }
+
+        url = protocol + "://" + hostname + ":" + port + url + "/command/toggleSequentialDownload";
+
+        // New JSONObject request
+        StringRequest jsArrayRequest = new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("Debug", "===Command===");
+                        Log.d("Debug", "Response: " + response);
+
+                        Log.d("Debug", "hashes: " + hashes);
+
+                        // Return value
+                        callback.onSuccess("");
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+
+                        Log.d("Debug", "Error in JSON response: " + error.getMessage());
+
+
+                        Toast.makeText(getApplicationContext(), "Error executing command: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }
+        ) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("User-Agent", "qBittorrent for Android");
+                params.put("Host", protocol + "://" + hostname + ":" + port);
+                params.put("Referer", protocol + "://" + hostname + ":" + port);
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                params.put("Cookie", cookie);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("hashes", hashes);
+                return params;
+            }
+        };
+
+        // Add request to te queue
+        addVolleyRequest(jsArrayRequest);
+
+    }
+
+
 
     // Wraps
     private void getApi() {
@@ -2582,9 +2652,9 @@ public class MainActivity extends AppCompatActivity implements RefreshListener {
 
     }
 
-    public void toggleFirstLastPiecePriority(String hashes) {
+    public void toggleFirstLastPiecePrio(String hashes) {
 
-        toggleFirstLastPiecePriority(hashes, new VolleyCallback() {
+        toggleFirstLastPiecePrio(hashes, new VolleyCallback() {
             @Override
             public void onSuccess(String result) {
 
@@ -2598,7 +2668,27 @@ public class MainActivity extends AppCompatActivity implements RefreshListener {
             }
         });
 
-    }    // End of wraps
+    }
+
+    public void toggleSequentialDownload(String hashes) {
+
+        toggleSequentialDownload(hashes, new VolleyCallback() {
+            @Override
+            public void onSuccess(String result) {
+
+                Log.d("Debug: ", ">>> toggleSequentialDownload: " + result);
+
+                toastText(R.string.priorityUpdated);
+
+                // Refresh
+                refreshAfterCommand(3);
+
+            }
+        });
+
+    }
+
+    // End of wraps
 
     private void refresh(String state, String label) {
 
@@ -3854,18 +3944,6 @@ public class MainActivity extends AppCompatActivity implements RefreshListener {
 
         // Delay of 3 seconds
         refreshAfterCommand(3);
-    }
-
-    public void toggleFirstLastPiecePrio(String hashes) {
-
-        toggleFirstLastPiecePriority(hashes);
-    }
-
-    public void toggleSequentialDownload(String hashes) {
-        // Execute the task in background
-        qBittorrentCommand qtc = new qBittorrentCommand();
-        qtc.execute(new String[]{"toggleSequentialDownload", hashes});
-
     }
 
     public void setLabel(String hashes, String label) {
